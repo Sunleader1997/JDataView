fbbb58bdd85791706df22c0bb803d9144c068fbd
sunleader <sunyaxing_1997@163.com>
2025-12-23 20:02:20 +0800

    解决windows 开发环境下运行失败

--- JDataViewServer/src/main/java/org/sunyaxing/imagine/jdataviewserver/cli/JDataViewCli.java
+++ JDataViewServer/src/main/java/org/sunyaxing/imagine/jdataviewserver/cli/JDataViewCli.java
@@ -7,37 +7,46 @@ import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Configuration;
+import org.sunyaxing.imagine.jdataviewserver.controller.JavaAppController;
 
+import java.util.List;
 import java.util.Set;
 
 @Configuration
 public class JDataViewCli implements ApplicationRunner {
 
+    @Autowired
+    private JavaAppController javaAppController;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.setProperty("java.awt.headless", "false");
-        Terminal terminal = new DefaultTerminalFactory().createTerminal();
-        Screen screen = new TerminalScreen(terminal);
-        screen.startScreen();
-        MultiWindowTextGUI gui = new MultiWindowTextGUI(screen);
-        gui.updateScreen();
-        BasicWindow window = new BasicWindow("JDataView CLI");
-        window.setHints(Set.of(
-                Window.Hint.CENTERED,
-                Window.Hint.NO_POST_RENDERING,
-                Window.Hint.EXPANDED,
-                Window.Hint.FULL_SCREEN
-        ));
-        Component mainPanel = createMainPanel();
-        window.setComponent(mainPanel);
-        gui.addWindow(window);
-        gui.addWindowAndWait(window); // 阻塞等待窗口关闭
+        List<String> modes = args.getOptionValues("mode");
+        if (modes != null && !modes.isEmpty() && modes.get(0).equals("cli")) {
+            System.setProperty("java.awt.headless", "false");
+            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+            screen.startScreen();
+            MultiWindowTextGUI gui = new MultiWindowTextGUI(screen);
+            gui.updateScreen();
+            BasicWindow window = new BasicWindow("JDataView CLI");
+            window.setHints(Set.of(
+                    Window.Hint.CENTERED,
+                    Window.Hint.NO_POST_RENDERING,
+                    Window.Hint.EXPANDED,
+                    Window.Hint.FULL_SCREEN
+            ));
+            Component mainPanel = createMainPanel();
+            window.setComponent(mainPanel);
+            gui.addWindow(window);
+            gui.addWindowAndWait(window); // 阻塞等待窗口关闭
+        }
     }
 
-    private static Panel createMainPanel() {
+    private Panel createMainPanel() {
         Panel panel = new Panel();
         panel.setLayoutManager(new BorderLayout());
         // Create title bar
@@ -50,7 +59,7 @@ public class JDataViewCli implements ApplicationRunner {
         return panel;
     }
 
-    private static Panel createTitleBar() {
+    private Panel createTitleBar() {
         Panel titleBar = new Panel();
         titleBar.setLayoutManager(new BorderLayout());
 
@@ -69,14 +78,14 @@ public class JDataViewCli implements ApplicationRunner {
         return titleBar;
     }
 
-    private static Panel createAppListPanel() {
+    private Panel createAppListPanel() {
         Panel listPanel = new Panel();
         listPanel.setLayoutManager(new LinearLayout(Direction.VERTICAL));
 
-        Table<String> table = new Table<String>("Column 1", "Column 2", "Column 3");
-//        javaAppController.getJavaApps().getData().forEach(javaAppDto -> {
-//            table.getTableModel().addRow(javaAppDto.getAppName(), "2", "3");
-//        });
+        Table<String> table = new Table<String>("Application", "Status", "Attached");
+        javaAppController.getJavaApps().getData().forEach(javaAppDto -> {
+            table.getTableModel().addRow(javaAppDto.getAppName(), javaAppDto.isAlive() ? "Alive" : "Dead", javaAppDto.isHasAttached() ? "Attached" : "Detached");
+        });
         listPanel.addComponent(table);
         return listPanel;
     }
